665f8306046b46b8569cd386b51a19cbdf275683
feldafahmi <feldafahmi@gmail.com>
2026-06-16 17:03:15 +0700

    seeder untuk akun

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,32 +2,54 @@
 
 namespace Database\Seeders;
 
-use App\Models\User;
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
 
-class DatabaseSeeder extends Seeder
+class UserSeeder extends Seeder
 {
-    use WithoutModelEvents;
-
     /**
-     * Seed the application's database.
+     * Run the database seeds.
      */
     public function run(): void
     {
-        // User::factory(10)->create();
-
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+        // 1. Membuat Akun Admin (Root Kasta Tertinggi)
+        User::create([
+            'name' => 'Admin MARK-UP',
+            'username' => 'adminmarkup',
+            'first_name' => 'Admin',
+            'last_name' => 'MARK-UP',
+            'email' => 'admin@markup.com',
+            'password' => Hash::make('passwordadmin'), // Otomatis enkripsi password bcrypt
+            'role' => 'admin',
+            'institution' => 'Universitas Airlangga',
+            'department' => 'Sistem Informasi',
         ]);
 
-        $this->call([
-            UserSeeder::class,
-            ProductSeeder::class,
-            MentorSeeder::class,
-            ProductDetailSeeder::class,
-            ReviewSeeder::class,
+        // 2. Membuat Akun Mentor (Kasta Menengah)
+        User::create([
+            'name' => 'Mentor MARK-UP',
+            'username' => 'mentormarkup',
+            'first_name' => 'Mentor',
+            'last_name' => 'Markup',
+            'email' => 'mentor@markup.com',
+            'password' => Hash::make('passwordmentor'),
+            'role' => 'mentor',
+            'institution' => 'Universitas Airlangga',
+            'department' => 'Sistem Informasi',
         ]);
+
+        // 3. Membuat Akun User/Student biasa (Kasta Dasar)
+        // User::create([
+        //     'name' => 'User MARK-UP',
+        //     'username' => 'usermarkup',
+        //     'first_name' => 'User',
+        //     'last_name' => 'Markup',
+        //     'email' => 'user@markup.com',
+        //     'password' => Hash::make('passworduser'),
+        //     'role' => 'user',
+        //     'institution' => 'Universitas Airlangga',
+        //     'department' => 'Sistem Informasi',
+        // ]);
     }
 }
